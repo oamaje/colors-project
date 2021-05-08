89f5e07f8cd5954b313ab0c31cc79ea8916e61c4
Payam Abubakr Abdullah <Payam97abubakr@gmail.com>
2021-05-08 21:21:46 +0300

    the for loop for random colors is done, but have some issus with showing the hexa code

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,24 @@
-const color1 = document.getElementById("colorOne");
-const color2 = document.getElementById("colorTwo");
-const color3 = document.getElementById("colorThree");
-const color4 = document.getElementById("colorFour");
-const color5 = document.getElementById("colorFive"); 
+// const color1 = document.getElementById("colorOne");
+// const color2 = document.getElementById("colorTwo");
+// const color3 = document.getElementById("colorThree");
+// const color4 = document.getElementById("colorFour");
+// const color5 = document.getElementById("colorFive"); 
 const gencolor = document.getElementById("generate");
 
-const color1para = document.getElementById("coloronep"); 
+// const color1para = document.getElementById("coloronep"); 
+
+var divs = document.querySelectorAll('div');
+
+
+
 
 
 gencolor.addEventListener("click",function () {
-    const randcol = () => {
-        const randomColor1 = Math.floor(Math.random()*16777215).toString(16);
-        const randomColor2 = Math.floor(Math.random()*16777215).toString(16);
-        const randomColor3 = Math.floor(Math.random()*16777215).toString(16);
-        const randomColor4 = Math.floor(Math.random()*16777215).toString(16);
-        const randomColor5 = Math.floor(Math.random()*16777215).toString(16);
-        color1.style.backgroundColor = "#" + randomColor1;
-        color2.style.backgroundColor = "#" + randomColor2;
-        color3.style.backgroundColor = "#" + randomColor3;
-        color4.style.backgroundColor = "#" + randomColor4;
-        color5.style.backgroundColor = "#" + randomColor5;
+    divs.forEach(function(item) {
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        item.style.backgroundColor = "#"+ randomColor;
+        let hexaColor=document.createElement("p");
+        hexaColor.innerText = "# "+ randomColor;
 
-        
-        color1para.innerText = "# "+ randomColor1;
-
-        }
-        randcol();
-
-
+    });
 })
