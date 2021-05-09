baf5f44c1ea44393803e1cf460482e18f1d6849a
Payam Abubakr Abdullah <Payam97abubakr@gmail.com>
2021-05-10 00:54:31 +0300

    save and library button are working now, we just need to do some design to show the them

--- scripts/script.js
+++ scripts/script.js
@@ -4,25 +4,43 @@ let color = document.querySelectorAll(".color");
 
 let lock=document.querySelectorAll(".lock")
 let generate=document.getElementById("generate")
+let saveBtn=document.getElementById("save")
+let lib=document.getElementById("library")
 
-
-
+//we will use this array to store the colors into local storage 
+let array=[];
+let randomColor="";
 generate.addEventListener("click", () => {
+
     for (let i = 0; i < 5; i++) {
       if(lock[i].classList.contains("fa-unlock")){
-        code[i].innerText = "#"+Math.floor(Math.random() * 16777215).toString(16);
-        color[i].style.backgroundColor = "#"+ Math.floor(Math.random() * 16777215).toString(16);
+          randomColor=Math.floor(Math.random() * 16777215).toString(16);
+        code[i].innerText = "#"+randomColor
+        color[i].style.backgroundColor = "#"+ randomColor;
+     array[i]=randomColor;
       }
     }
   });
+  console.log(array);
 
 lock.forEach(locking =>{
     locking.addEventListener("click", ()=>{
       lockedDiv(locking);
     })
   })
+
+  saveBtn.addEventListener("click",()=>{
+    localStorage.setItem("colorpalette", JSON.stringify(array));
+  })
+
   
-  
+  lib.addEventListener("click",()=>{
+    var retrievedData = localStorage.getItem("colorpalette");
+    console.log(retrievedData);
+  })
+
+
+  ///switch and case 
   const lockedDiv = (locked)=>{
     if(locked.classList.contains("fa-unlock")){
       locked.classList.remove("fa-unlock");
