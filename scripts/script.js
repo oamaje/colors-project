a0533804d721aebbbd6578521fc4e7c1204d5e6c
omar-bassam-ammaj <oamaje@gmail.com>
2021-05-09 11:24:23 +0300

    lock WIP

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,31 @@
+let code = document.querySelectorAll(".code");
 
-var code = document.querySelectorAll('.code');
+let color = document.querySelectorAll(".color");
 
-var color = document.querySelectorAll('.color');
+let button = document.querySelector(".generate");
 
-var button = document.querySelector('.generate');
+let lock = document.getElementById("lock");
 
-button.addEventListener("click",colorGen);
+button.addEventListener("click", colorGen);
+lock.addEventListener("click",
+function checklock() {
+    for (let index = 0; index < 5; index++) {
+     lock.value = true;    
+     console.log(lock.value);   
+    }
+})
 
 function colorGen() {
-
-
-    for (let i=0;i<code.length;i++){
-        const randomColor = Math.floor(Math.random()*16777215).toString(16);
-        code[i].innerHTML = "#"+Math.floor(Math.random()*16777215).toString(16);;
-
-        color[i].style.backgroundColor = "#"+randomColor;
-
-    }
-
+  for (let i = 0; i < code.length; i++) {
+      if (lock.value == true) {
+          console.log("locked");
+      } else if (lock.value == false ) {
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        code[i].innerHTML = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        color[i].style.backgroundColor = "#" + randomColor;
+      }
+    
+  }
 }
-
 colorGen();
+
