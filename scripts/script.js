e51d14eb2d93e825beaf717feabfeb2b72948bda
omar-bassam-ammaj <oamaje@gmail.com>
2021-05-10 14:24:28 +0300

    done with everything execpt the library stack

--- scripts/script.js
+++ scripts/script.js
@@ -11,12 +11,14 @@ let lib = document.getElementById("library");
 let array = [];
 let randomColor = "";
 generate.addEventListener("click", () => {
+
   for (let i = 0; i < 5; i++) {
     if (lock[i].classList.contains("fa-unlock")) {
       randomColor = Math.floor(Math.random() * 16777215).toString(16);
       code[i].innerText = "#" + randomColor;
       color[i].style.backgroundColor = "#" + randomColor;
       array[i] = randomColor;
+      
     }
   }
 });
@@ -28,9 +30,9 @@ lock.forEach((locking) => {
   });
 });
 
-saveBtn.addEventListener("click", () => {
-  localStorage.setItem("colorpalette", JSON.stringify(array));
-});
+// saveBtn.addEventListener("click", () => {
+//   localStorage.setItem("colorpalette", JSON.stringify(array));
+// });
 
 lib.addEventListener("click", () => {
   var retrievedData = localStorage.getItem("colorpalette");
@@ -64,7 +66,7 @@ const savespan = document.getElementById("closebtn");
 const savewindow = document.getElementById("modal");
 const libwindow = document.getElementById("libmodal");
 const libspan = document.getElementById("libclosebtn");
-const librarypallets = document.getElementById("colorpalletes");
+const librarypallets = document.getElementById("pname");
 const savepabtn = document.getElementById("savepallete");
 
 //showing the library modal
@@ -102,4 +104,32 @@ function saveshow() {
 saveshow();
 libraryshow();
 
-savepabtn.append(array);
+const div = document.createElement("div");
+librarypallets.append(div);
+
+const arrayname = document.getElementById("saveinp");
+
+savepallete.addEventListener("click", () => {
+  localStorage.setItem(arrayname.value, JSON.stringify(array));
+ 
+const c1 = document.getElementById("c1")
+const c2 = document.getElementById("c2")
+const c3 = document.getElementById("c3")
+const c4 = document.getElementById("c4")
+const c5 = document.getElementById("c5")
+
+
+c1.style.backgroundColor = "#"+array[0];
+c2.style.backgroundColor = "#"+array[1];
+c3.style.backgroundColor = "#"+array[2];
+c4.style.backgroundColor = "#"+array[3];
+c5.style.backgroundColor = "#"+array[4];
+c1.innerText = array[0];
+c2.innerText = array[1];
+c3.innerText = array[2];
+c4.innerText = array[3];
+c5.innerText = array[4];
+
+div.innerHTML ="<b > " + arrayname.value + " </b> " +"<br>";
+div.style.color = "#114d64";
+});
